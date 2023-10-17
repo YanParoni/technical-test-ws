3451b7b0b22919a7827e6049d8aa92d7899281e0
YanParoni <paroniyan@gmail.com>
2023-10-17 04:12:39 -0300

    fix: deploy issues fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,7 @@
 import Carousel from 'lib/client/components/Carousel';
 import Hero from 'lib/client/components/Hero';
 import ProductCard from 'lib/client/components/ProductCard';
-import { NextResponse } from 'next/server';
 import { useDerivedState } from 'lib/client/hooks';
-
-export function middleware(request: Request) {
-  const url = new URL(request.url);
-  const origin = url.origin;
-  const pathname = url.pathname;
-  const requestHeaders = new Headers(request.headers);
-  requestHeaders.set('x-url', request.url);
-  requestHeaders.set('x-origin', origin);
-  requestHeaders.set('x-pathname', pathname);
-
-  return NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  });
-}
 
 export default function Home() {
   const derivedState = useDerivedState();
